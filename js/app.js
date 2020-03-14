12b10c3b148ae06d0465d51a0b661ca707afd47d
Dawid Zdrojewski <dawid.zdrojewskI@epaka.pl>
2020-03-14 22:03:06 +0100

    layout

--- js/app.js
+++ js/app.js
@@ -1,100 +1,97 @@
 function setOtherCountries() {
-    var urlOtherCountries = 'https://services9.arcgis.com/N9p5hsImWXAccRNI/ArcGIS/rest/services/Z7biAeD8PAkqgmWhxG2A/FeatureServer/2/query?where=OBJECTID+%3E+0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=Country_region%2CConfirmed%2CDeaths%2CRecovered&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=Confirmed+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=false&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
-
+    let urlOtherCountries = 'https://services9.arcgis.com/N9p5hsImWXAccRNI/ArcGIS/rest/services/Z7biAeD8PAkqgmWhxG2A/FeatureServer/2/query?where=OBJECTID+%3E+0&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=Country_region%2CConfirmed%2CDeaths%2CRecovered&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=Confirmed+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=false&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
 
     $.get(urlOtherCountries, function (data) {
         let covidData = JSON.parse(data);
 
-        appendData(covidData);
+        let infected = covidData.features[0].properties.Confirmed;
+        let deaths = covidData.features[0].properties.Deaths;
+        let cured = covidData.features[0].properties.Recovered;
+        let curedPercentage = parseFloat(cured*100/infected).toFixed(2);
+        let deathsPercentage = parseFloat(deaths*100/infected).toFixed(2);
+
+        covidData.features.forEach(function (value, index, array) {
+            let country = value.properties.Country_Region;
+            let infected = value.properties.Confirmed;
+            let deaths = value.properties.Deaths;
+            let cured = value.properties.Recovered;
+            let curedPercentage = parseFloat(cured*100/infected).toFixed(2);
+            let deathsPercentage = parseFloat(deaths*100/infected).toFixed(2);
+
+            $('.otherCountries').append(
+                '<tr>' +
+                '   <td>'+country+'</td>' +
+                '   <td>'+infected+'</td>' +
+                '   <td>'+curedPercentage+' %</td>' +
+                '   <td>'+cured+'</td>' +
+                '   <td>'+deathsPercentage+' %</td>' +
+                '   <td>'+deaths+'</td>' +
+                '</tr>'
+            );
+        });
     });
 
 }
 
 function setPolishData() {
-    var url = 'https://services1.arcgis.com/YmCK8KfESHdxUQgm/ArcGIS/rest/services/KoronawirusPolska_czas/FeatureServer/0/query?where=Potwierdzone+%3E+0&objectIds=&time=&resultType=none&outFields=Potwierdzone%2CWyleczone%2CSmiertelne&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Potwierdzone+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=1&sqlFormat=none&f=pgeojson&token=';
-
-    var cookieCovid = checkCookie('arcgisPolishData');
-
-    if(cookieCovid !== false) {
-        appendPolishData(cookieCovid);
-        return false;
-    }
+    let url = 'https://services1.arcgis.com/YmCK8KfESHdxUQgm/ArcGIS/rest/services/KoronawirusPolska_czas/FeatureServer/0/query?where=Potwierdzone+%3E+0&objectIds=&time=&resultType=none&outFields=Potwierdzone%2CWyleczone%2CSmiertelne&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Potwierdzone+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=1&sqlFormat=none&f=pgeojson&token=';
 
     $.get(url, function (data) {
-        var covidPolishData = JSON.parse(data);
+        let covidPolishData = JSON.parse(data);
 
-        setCookiePolish(covidPolishData);
-        appendPolishData(covidPolishData);
+        infectedPL = covidPolishData.features[0].properties.Potwierdzone;
+        deathsPL = covidPolishData.features[0].properties.Smiertelne;
+        curedPL = covidPolishData.features[0].properties.Wyleczone;
+        curedPercentagePL = parseFloat(curedPL*100/infectedPL).toFixed(2);
+        deathsPercentagePL = parseFloat(deathsPL*100/infectedPL).toFixed(2);
+
+        $('#infected').append(infectedPL);
+        $('#deaths').append(deathsPL);
+        $('#deathsPercentage').append(deathsPercentagePL+' %');
+        $('#cured').append(curedPL);
+        $('#curedPercentage').append(curedPercentagePL+ ' %');
+        scroll();
     });
 }
 
-function appendData(covidData) {
-    covidData.features.forEach(function (value, index, array) {
-        let country = value.properties.Country_Region;
-        let infected = value.properties.Confirmed;
-        let deaths = value.properties.Deaths;
-        let cured = value.properties.Recovered;
-        let curedPercentage = parseFloat(cured*100/infected).toFixed(2);
-        let deathsPercentage = parseFloat(deaths*100/infected).toFixed(2);
+function setMargins(){
+    $('.number, .kraje').css('margin-top', $('.main-head').outerHeight());
+}
 
-        $('.otherCountries').append(
-            '<tr>' +
-            '<td>'+country+'</td>' +
-            '<td>'+infected+'</td>' +
-            '<td>'+curedPercentage+' %</td>' +
-            '<td>'+cured+'</td>' +
-            '<td>'+deathsPercentage+' %</td>' +
-            '<td>'+deaths+'</td>' +
-            '</tr>'
-        );
+function scroll(elemId = 'step1'){
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#"+elemId).offset().top
+    }, 1000);
+
+    switch (elemId){
+        case 'step1':
+            var c = new CountUp("infected",0,infectedPL);
+            break;
+        case 'step2':
+            var c = new CountUp("cured",0,curedPL);
+            break;
+        case 'step3':
+            var c = new CountUp("deaths",0,deathsPL);
+            break;
+        default:
+            return false;
+    }
+
+    c.start();
+}
+
+function setUpMenu(){
+    $('.menu-bullet').click(function(e){
+        e.preventDefault();
+        var step = $(this).data('step');
+        console.log(step);
+        scroll('step'+step);
     });
-}
-
-function appendPolishData(covidPolishData) {
-    let infected = covidPolishData.features[0].properties.Potwierdzone;
-    let deaths = covidPolishData.features[0].properties.Smiertelne;
-    let cured = covidPolishData.features[0].properties.Wyleczone;
-    let curedPercentage = parseFloat(cured*100/infected).toFixed(2);
-    let deathsPercentage = parseFloat(deaths*100/infected).toFixed(2);
-
-    $('.infected').append(infected);
-    $('.deaths').append(deaths);
-    $('.deathsPercentage').append(deathsPercentage+' %');
-    $('.cured').append(cured);
-    $('.curedPercentage').append(curedPercentage+ ' %');
-}
-
-function setCookiePolish(covidData) {
-    var date = new Date();
-    var minutes = 10;
-    date.setTime(date.getTime() + (minutes * 60 * 1000));
-
-    $.cookie('arcgisPolishData', JSON.stringify(covidData), {expires: date});
-}
-
-function checkCookie(cookieName = 'arcgisData') {
-    if($.cookie(cookieName) === undefined) {
-        return false;
-    }
-
-    return JSON.parse($.cookie(cookieName));
-}
-
-function checkVisited() {
-    var cookie = $.cookie('visited');
-
-    if(cookie === undefined) {
-        $('.modal-content').show();
-    }
-}
-
-var setVisited = function () {
-    $.cookie('visited', 'yes', {expires: 180});
-    $('.modal-content').hide();
 }
 
 $(document).ready(function () {
-    checkVisited();
     setPolishData();
     setOtherCountries();
+    setMargins();
+    setUpMenu();
 });
